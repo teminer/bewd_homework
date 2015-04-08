c9137e9dfb39a74254ad91999ee9cde8cd6bb4c6
tam <tamma.miner@gmail.com>
2015-04-07 22:23:24 -0700

    Sanitized .downsize calls & used simple comma delimiting without trailing comma

--- animal_sounds.rb
+++ animal_sounds.rb
@@ -1,10 +1,10 @@
 puts "Please enter the name of an animal that makes an animal sound.\n"
-animal = gets.chomp
+animal = gets.chomp.downcase
 
-puts "What sound does a " + animal.downcase + " make?\n"
-sound = gets.chomp + ", "
+puts "What sound does a " + animal + " make?\n"
+sound = gets.chomp
 
 puts "How many times would you like the " + animal + " to vocalize this?"
 multiplier = gets.chomp.to_i 
 
-puts "The " + animal.downcase + " goes " + sound.downcase*multiplier
+puts "The " + animal + " goes " + (sound + ", ") * (multiplier - 1) + sound
