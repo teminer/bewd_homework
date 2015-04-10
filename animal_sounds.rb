323a3bd52c94347f97a5a51f17215823cc7ebc47
tam <tamma.miner@gmail.com>
2015-04-09 20:25:24 -0700

    Cleaned code to replace concatenated strings with interpolated strings

--- animal_sounds.rb
+++ animal_sounds.rb
@@ -1,10 +1,12 @@
 puts "Please enter the name of an animal that makes an animal sound.\n"
 animal = gets.chomp.downcase
 
-puts "What sound does a " + animal + " make?\n"
+puts "What sound does a #{animal} make?\n"
 sound = gets.chomp
 
-puts "How many times would you like the " + animal + " to vocalize this?"
+puts "How many times would you like the #{animal} to vocalize this?"
 multiplier = gets.chomp.to_i 
 
-puts "The " + animal + " goes " + (sound + ", ") * (multiplier - 1) + sound
+sounds = (sound + ",") * (multiplier - 1)
+
+puts "The #{animal} goes #{sounds} #{sound}"
