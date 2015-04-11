1841b0c25a1698df9b82b789416b72bc710b82dd
tam <tamma.miner@gmail.com>
2015-04-11 10:14:08 -0700

    99 bottles

---
+++ 99_bottles.rb
@@ -0,0 +1,13 @@
+num_bottles = 99
+
+while num_bottles > 1 
+	puts "#{num_bottles} bottles of beer on the wall, #{num_bottles} of beer."
+	num_bottles = num_bottles - 1
+	puts "Take one down and pass it around, #{num_bottles} of beer on the wall."
+end
+
+if num_bottles = 0
+	puts "No more bottles of beer on the wall, no more bottles of beer."
+	puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+else
+end
