4f5e0df4bd0e20eac5be89c86c3dbcb5bb533646
tam <tamma.miner@gmail.com>
2015-04-24 00:29:06 -0700

    returns full list

--- class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
+++ class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
@@ -3,13 +3,17 @@ require 'httparty'
 usa_books =HTTParty.get('http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key=anwnn862kgvf2zsbey7bm9ay')
 parsed = usa_books.parsed_response
 
+#title = parsed['BookLists'][0]['BookListEntries'][0]['Title']
+#puts title
 
-booklists = parsed['BookLists']
+most_recent_booklists = parsed['BookLists'][0]['BookListEntries']
 
 #output title, author & brief description of the first 20 books from most recently published list
-booklists.each do |booklist_hash|
+most_recent_booklists.each do |booklist_hash|
 
-	puts "#{booklist_hash['Title']} by #{booklist_hash['author']}"
+	puts "#{booklist_hash['Title']} by #{booklist_hash['Author']}"
+	puts "#{booklist_hash['BriefDescription']}"
+	puts "\n------------------------------------------------------\n"
 	
 end
 
