4f7eab2aa7da83de0123d61ebc64cab2972ce79a
tam <tamma.miner@gmail.com>
2015-04-24 00:41:20 -0700

    limit list to top 20

--- class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
+++ class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
@@ -9,11 +9,12 @@ parsed = usa_books.parsed_response
 most_recent_booklists = parsed['BookLists'][0]['BookListEntries']
 
 #output title, author & brief description of the first 20 books from most recently published list
-most_recent_booklists.each do |booklist_hash|
-
-	puts "#{booklist_hash['Title']} by #{booklist_hash['Author']}"
-	puts "#{booklist_hash['BriefDescription']}"
-	puts "\n------------------------------------------------------\n"
-	
+most_recent_booklists.each_with_index do |booklist_hash,i|
+	if i<20
+		puts "#{i+1}. #{booklist_hash['Title']} by #{booklist_hash['Author']}"
+		puts "#{booklist_hash['BriefDescription']}"
+		puts "\n------------------------------------------------------\n"
+		i=i+1
+	end
 end
 
