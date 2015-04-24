4cf7b415ed1b45773f8b75b2d63beddddc1319ab
tam <tamma.miner@gmail.com>
2015-04-24 12:44:44 -0700

    Moved count & class to api call

--- class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
+++ class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 
-usa_books =HTTParty.get('http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key=anwnn862kgvf2zsbey7bm9ay')
+usa_books =HTTParty.get('http://api.usatoday.com/open/bestsellers/books/ThisWeek?count=20&class=Fiction&api_key=anwnn862kgvf2zsbey7bm9ay')
 parsed = usa_books.parsed_response
 
 #title = parsed['BookLists'][0]['BookListEntries'][0]['Title']
@@ -9,19 +9,17 @@ parsed = usa_books.parsed_response
 most_recent_booklists = parsed['BookLists'][0]['BookListEntries']
 
 #output title, author & brief description of the first 20 books from most recently published list
-genre = parsed['BookLists'][0]['BookListEntries']['Class']
+#genre = parsed['BookLists'][0]['BookListEntries']
 
 most_recent_booklists.each_with_index do |booklist_hash,i|
-	if i<20 && genre == 'Fiction'
 		puts "#{i+1}. #{booklist_hash['Title']} by #{booklist_hash['Author']}"
 		null_description = parsed['BookLists'][0]['BriefDescription']
-		if null_description == true
-			puts "#{booklist_hash['BriefDescription']}"
-		else
-			puts "This is a good book!"
-		end
+			if null_description == true
+				puts "#{booklist_hash['BriefDescription']}"
+			else
+				puts "This is a good book!"
+			end
 		puts "\n------------------------------------------------------\n"
 		i=i+1
 	end
-end
 
