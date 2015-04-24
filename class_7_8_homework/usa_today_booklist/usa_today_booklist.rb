27d04c0a3e67f6d831f86b97f604ede6d44b2728
tam <tamma.miner@gmail.com>
2015-04-24 13:00:42 -0700

    Added error handling if BookLists is empty

--- class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
+++ class_7_8_homework/usa_today_booklist/usa_today_booklist.rb
@@ -8,18 +8,19 @@ parsed = usa_books.parsed_response
 
 most_recent_booklists = parsed['BookLists'][0]['BookListEntries']
 
-#output title, author & brief description of the first 20 books from most recently published list
-#genre = parsed['BookLists'][0]['BookListEntries']
-
-most_recent_booklists.each_with_index do |booklist_hash,i|
-		puts "#{i+1}. #{booklist_hash['Title']} by #{booklist_hash['Author']}"
-		null_description = parsed['BookLists'][0]['BriefDescription']
-			if null_description == true
-				puts "#{booklist_hash['BriefDescription']}"
-			else
-				puts "This is a good book!"
-			end
-		puts "\n------------------------------------------------------\n"
-		i=i+1
+if most_recent_booklists.empty?
+	puts "There are no Bestsellers this week. Try the library!\n"
+else
+	#output title, author & brief description of the first 20 books from most recently published list
+	most_recent_booklists.each_with_index do |booklist_hash,i|
+			puts "#{i+1}. #{booklist_hash['Title']} by #{booklist_hash['Author']}"
+			null_description = parsed['BookLists'][0]['BriefDescription']
+				if null_description == true
+					puts "#{booklist_hash['BriefDescription']}"
+				else
+					puts "This is a good book!"
+				end
+			puts "\n------------------------------------------------------\n"
+			i=i+1
 	end
-
+end
