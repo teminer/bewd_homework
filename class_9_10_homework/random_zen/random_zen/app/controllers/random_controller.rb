26bdf16aaae221b32122b27247fff19d3d6b5b66
tam <tamma.miner@gmail.com>
2015-05-01 00:01:45 -0700

    added hacky lorem image

--- class_9_10_homework/random_zen/random_zen/app/controllers/random_controller.rb
+++ class_9_10_homework/random_zen/random_zen/app/controllers/random_controller.rb
@@ -3,6 +3,7 @@ class RandomController < ApplicationController
 	def zen
 
 		@quote = HTTParty.get('https://api.github.com/zen?access_token=bd22e26e5f04873ca2511afb7b67fbe946a084fc')
+		@pic = HTTParty.get('http://lorempixel.com/400/200/nature/')
 
 	end
 
